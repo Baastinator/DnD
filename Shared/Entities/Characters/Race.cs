ce7757d87cfc2ff82e1908d8c3146c3f26c5ba4d
SebastianKring <sebastian.kring@stud.bs7-augsburg.de>
2021-11-22 17:08:29 +0100

    AAAAAAAAAAAAAAA

--- Shared/Entities/Characters/Race.cs
+++ Shared/Entities/Characters/Race.cs
@@ -18,15 +18,25 @@ namespace DND.Shared.Entities.Characters
         private static int[] GreenHairColors = {30, 0, 100, 0, 0, 0, 10};
         private static int[] TieflingHairColors = {30, 0, 10, 50, 30, 0, 0};
 
+        public Race()
+        {
+            SkinColorWeights = DefaultSkinWeights;
+        }
+
         public static Race Human = new Race {ID = 0, Name = "Human", StatBonus = new[] {1, 1, 1, 1, 1, 1}};
         public static Race Elf = new Race {ID = 1, Name = "Elf", StatBonus = new[] {0, 2, 0, 1, 0, 0}};
         public static Race Dwarf = new Race {ID = 2, Name = "Dwarf", StatBonus = new[] {0, 0, 2, 0, 0, 0}};
         public static Race HalfElf = new Race {ID = 3, Name = "HalfElf", StatBonus = new[] {0, 1, 0, 0, 1, 2}};
         public static Race Gnome = new Race {ID = 4, Name = "Gnome", StatBonus = new[] {0, 0, 1, 2, 0, 0}};
-        public static Race HalfOrc = new Race {ID = 5, Name = "HalfOrc", StatBonus = new[] {2, 0, 1, 0, 0, 0}};
+
+        public static Race HalfOrc = new Race
+            {ID = 5, Name = "HalfOrc", StatBonus = new[] {2, 0, 1, 0, 0, 0}, SkinColorWeights = GreenSkinWeights};
         public static Race Halfling = new Race {ID = 6, Name = "Halfling", StatBonus = new[] {0, 2, 0, 0, 0, 0}};
+
         public static Race Dragonborn = new Race {ID = 7, Name = "Dragonborn", StatBonus = new[] {2, 0, 0, 0, 0, 1}};
-        public static Race Tiefling = new Race {ID = 8, Name = "Tiefling", StatBonus = new[] {0, 0, 0, 0, 0, 2}};
+
+        public static Race Tiefling = new Race
+            {ID = 8, Name = "Tiefling", StatBonus = new[] {0, 0, 0, 0, 0, 2}, SkinColorWeights = TieflingSkinWeights};
         public static Race Goliath = new Race {ID = 9, Name = "Goliath", StatBonus = new[] {2, 0, 1, 0, 0, 0}};
         public static Race Tabaxi = new Race {ID = 10, Name = "Tabaxi", StatBonus = new[] {0, 0, 2, 0, 0, 1}};
         public static Race Genasi = new Race {ID = 11, Name = "Genasi", StatBonus = new[] {0, 0, 2, 0, 0, 0}};
